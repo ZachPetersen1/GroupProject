51c49851c34efb7f87e3f1400bccfb82ab951088
petersez <zach.petersen1@gmail.com>
2017-08-22 17:25:16 -0500

    Added code to get basic functions of calculator working

--- Team3GroupProject/Team3GroupProject.cpp
+++ Team3GroupProject/Team3GroupProject.cpp
@@ -9,7 +9,7 @@
 #include <string>
 #include <vector>
 using namespace std;
-
+string mathFunction(double, string, double);
 /*
 General Program Outline
 Take user input
@@ -20,11 +20,68 @@ Return the solution
 
 int main()
 {
-	int arrayLength;
 	vector<string> parsedInput;
-	
+	string userInput;
+	double result;
+
 	// Prompt user for equation
-	/*  (Copied from Mohamed's repository)
+	cout << " Enter in a mathematical expression  : " << endl; // Were asking the user to enter in a mathematical expression.
+
+	getline(cin, userInput); // This saves the user input of the users mathematical expression.
+
+	// Parse each number/operator into an array object (delimiters should be " ", "+", "-", "*", "/"...)
+
+	string sb = "";
+
+	for (unsigned int i = 0; i < userInput.length(); i++)
+	{
+		string currentChar = userInput.substr(i, 1);
+		//sb += currentChar;
+		if (currentChar == "+" || currentChar == "-" || currentChar == "/" || currentChar == "*" || currentChar == "(" || currentChar == ")")
+		{
+			parsedInput.push_back(sb);
+			parsedInput.push_back(currentChar);
+			sb = "";
+		}
+		else if (currentChar != " ")
+		{
+			sb += currentChar;
+		}
+
+	}
+	parsedInput.push_back(sb);
+
+	for (string s : parsedInput)
+		cout << s << endl;
+
+	// Iterate through array looking for operators
+
+	for (unsigned int i = 0; i < parsedInput.size(); i++)
+	{
+		// When an operator is found, pass it and it's neighboring elements to the mathFunction	
+
+
+		if (parsedInput[i] == "+" || parsedInput[i] == "-" || parsedInput[i] == "*" || parsedInput[i] == "/")
+		{
+			string result = mathFunction(stod(parsedInput[i - 1]), parsedInput[i], stod(parsedInput[i + 1]));
+			parsedInput.erase(parsedInput.begin() + i - 1);
+			parsedInput.erase(parsedInput.begin() + i - 1);
+			parsedInput[i - 1] = result;
+			i = 0;
+			// Replace 3 array elements with the method result
+
+			// Repeat until no operators exist and array length is 1
+		}
+
+	}
+
+
+
+	// Print the equation and solution for the user
+	cout << " The result of your mathematical expression equals to : " << parsedInput[0] << endl; // This will display the result of the mathematical expression.
+
+
+ /*  (Copied from Mohamed's repository)
 	string mathExpression;
 
 	string userInput;
@@ -41,57 +98,19 @@ int main()
 
 	if (userInput==mathExpression)
 	{
-		cout << " The number you enterd is a valid mathematical expression ";
+	cout << " The number you enterd is a valid mathematical expression ";
 	}
 
 	else {
 
-		cout << " The number you enterd is invalid. Please re-enter a valid mathematical expression";
+	cout << " The number you enterd is invalid. Please re-enter a valid mathematical expression";
 	}
 
 	cout << " The result of your mathematical expression equals to : " << result << endl; // This will display the result of the mathematical expression.
 
 
-	*/
+																						  */
 
-	
-	
-	// Parse each number/operator into an array object (delimiters should be " ", "+", "-", "*", "/"...)
-	
-
-	parsedInput.push_back("Test");
-	parsedInput.push_back("Test2");
-
-	
-	// Iterate through array looking for operators
-
-	for (int i = 0; i < parsedInput.size(); i++)
-	{
-		// When an operator is found, pass it and it's neighboring elements to the mathFunction	
-
-
-		if (parsedInput[i] == "+" || parsedInput[i] == "-" || parsedInput[i] == "*" || parsedInput[i] == "/")
-		{
-			string result = mathFunction(stod(parsedInput[i - 1]), (parsedInput[i]), stod(parsedInput[i + 1]));
-			parsedInput[i - 1].erase();
-			parsedInput[i + 1].erase();
-			parsedInput[i] = result;
-			i = 0;
-			// Replace 3 array elements with the method result
-
-			// Repeat until no operators exist and array length is 1
-		}
-
-	}
-
-	
-	// Print the equation and solution for the user
-	
-	
-	
-	cout << "Testing 1,2,3" << endl;
-	cout << "Testing 4,5,6" << endl;
-	//This is a placeholder
 }
 
 string mathFunction(double value1, string op, double value2)    /*declare variables needed for mathFunction, using string becuase it contains multiple characters*/
@@ -116,5 +135,5 @@ string mathFunction(double value1, string op, double value2)    /*declare variab
     {
         answer = value1 / value2;   // division equation
     }
-    return answer.to_string();      //casts answer variable to string mathFunction
+    return to_string(answer);      //casts answer variable to string mathFunction
 }
